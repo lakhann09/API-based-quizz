67db5ebeac900f4adca403c91357bfb470054bb4
Lakhan Patidar <lakhan.patidar0909@gmail.com>
2023-04-08 01:29:42 +0530

    upload

---
+++ data.py
@@ -0,0 +1,15 @@
+import json
+import requests
+number_of_question = int(input("Enter the number of question you want : "))
+
+url = f'https://opentdb.com/api.php?amount={number_of_question}&category=18&difficulty=easy&type=boolean'
+response = requests.get(url)
+data = response.text
+arranged_data = json.loads(data)
+
+question_data = arranged_data["results"]
+
+
+
+
+
